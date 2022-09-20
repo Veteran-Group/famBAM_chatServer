15589f29f53f4dbd9ea46815359ca17683b4bfd9
Robert E. Campbell Jr <robert.e.campbell.jr@gmail.com>
2022-09-19 21:56:22 -0600

    Basic chart up and running

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
 const httpServer = require("http").createServer();
 const port = 3002;
+const axios = require('axios');
 require('dotenv').config();
+const env = process.env;
 const io = require("socket.io")(httpServer, {
   cors: {
-    origin: ["http://localhost:3000", "http://192.168.1.8:3000", "http://192.168.1.12:*"],
+    origin: [env.LOCALADDRESS, env.REMOTEADDRESS],
     methods: ["GET", "POST"]
   }
 });
 
 /*
  *  socket.emit = to the client only
- *  socket.broadcast.emt = to everyone connected BUT the client
+ *  socket.broadcast.emit = to everyone connected BUT the client
  *  io.emit = to everyone
  */
 io.on('connection', (socket) => {
-  console.log('Fambam server connection established...');
+
+  socket.on('chat', (messagePack) => {
+    let newMessage = messagePack.newMessage;
+    let roomInfo = messagePack.roomInfo;
+    let profile = messagePack.profile;
+    let api = process.env.API;
+    io.emit('chat', newMessage);
+
+    axios.post(`${api}/newMessage?uid=${profile.id}&cid=${roomInfo.id}&un=${newMessage.user_name}&um=${newMessage.user_message}&ts=${newMessage.time_stamp}&da=${newMessage.date}`);
+  })
 
   socket.emit('message', 'Welcome to FamBAM chat server');
 
