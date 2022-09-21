9ce35ee4bc795829a0520a4f358f7c1575db1139
Robert E. Campbell Jr <robert.e.campbell.jr@gmail.com>
2022-09-20 19:14:12 -0600

    Chat rooms now completely function

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const httpServer = require("http").createServer();
 const port = 3002;
 const axios = require('axios');
+const { createMessage } = require('./lib/lib.js');
 require('dotenv').config();
 const env = process.env;
+const api = env.API;
 const io = require("socket.io")(httpServer, {
   cors: {
     origin: [env.LOCALADDRESS, env.REMOTEADDRESS],
@@ -10,25 +12,23 @@ const io = require("socket.io")(httpServer, {
   }
 });
 
+const botName = '-=FamBAM BOT=-';
+
 /*
  *  socket.emit = to the client only
  *  socket.broadcast.emit = to everyone connected BUT the client
  *  io.emit = to everyone
  */
 io.on('connection', (socket) => {
-
-  socket.on('chat', (messagePack) => {
-    let newMessage = messagePack.newMessage;
-    let roomInfo = messagePack.roomInfo;
-    let profile = messagePack.profile;
-    let api = process.env.API;
-    io.emit('chat', newMessage);
-
-    axios.post(`${api}/newMessage?uid=${profile.id}&cid=${roomInfo.id}&un=${newMessage.user_name}&um=${newMessage.user_message}&ts=${newMessage.time_stamp}&da=${newMessage.date}`);
+  socket.on('joinRoom', ({ roomName, username }) => {
+    socket.join(roomName);
+    socket.broadcast.to(roomName).emit('chat', createMessage(botName, `${username} has joined the channel`));
   })
 
-  socket.emit('message', 'Welcome to FamBAM chat server');
-
+  socket.on('chat', ({ newMessage, roomInfo, profile}) => {
+    io.in(roomInfo.roomName).emit('chat', newMessage)
+    axios.post(`${api}/newMessage?uid=${profile.id}&cid=${roomInfo.id}&un=${newMessage.user_name}&um=${newMessage.user_message}&ts=${newMessage.time_stamp}&da=${newMessage.date}`);
+  })
 })
 
 httpServer.listen(port, () => {
